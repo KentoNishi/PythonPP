9d072aff0c3d6a2ad781595da1739cbc0af21fbc
Kento Nishi <kento24gs@outlook.com>
2020-06-07 17:51:51 -0700

    Added pypi build task

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+import sys
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as reqs:
+    requirements = reqs.read().split("\n")
+
+versionName = sys.argv[1].replace("refs/tags/v", "")
+del sys.argv[1]
+
+setuptools.setup(
+    name="pythonpp",
+    version=versionName,
+    author="Ronak Badhe, Kento Nishi",
+    author_email="ronak.badhe@gmail.com, kento24gs@outlook.com",
+    description=long_description.split("\n")[1],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/r2dev2bb8/PythonPP",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.0",
+    install_requires=requirements,
+)
