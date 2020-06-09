30c8b1cd95792bd2c6f6bd4cb1cc891c444514e9
Kento Nishi <kento24gs@outlook.com>
2020-06-08 18:22:39 -0700

    Checkpoint

--- pythonpp/pythonpp.py
+++ pythonpp/pythonpp.py
@@ -3,15 +3,18 @@ import functools
 
 
 def __copyMethod(f):
-    g = types.FunctionType(
-        f.__code__,
-        f.__globals__,
-        name=f.__name__,
-        argdefs=f.__defaults__,
-        closure=f.__closure__,
-    )
-    g = functools.update_wrapper(g, f)
-    g.__kwdefaults__ = f.__kwdefaults__
+    try:
+        g = types.FunctionType(
+            f.__code__,
+            f.__globals__,
+            name=f.__name__,
+            argdefs=f.__defaults__,
+            closure=f.__closure__,
+        )
+        g = functools.update_wrapper(g, f)
+        g.__kwdefaults__ = f.__kwdefaults__
+    except AttributeError:
+        return lambda *args, **kwargs: None
     return g
 
 
@@ -28,10 +31,28 @@ def PythonPP(cls):
     copiedInit = __copyMethod(cls.__init__)
     staticPrivateScope = __PrivateScope()
 
-    class Scope:
+    class ScopeWrapper:
+        pass
+
+    class Scope(ScopeWrapper):
         def __init__(self, instance, static):
-            self.instance = instance
-            self.static = static
+            super().__setattr__("instance", instance)
+            super().__setattr__("static", static)
+
+        def __getattribute__(self, name):
+            try:
+                if name == "static":
+                    return super().__getattribute__("static")
+                return super().__getattribute__("instance").__getattribute__(name)
+            except AttributeError:
+                return super().__getattribute__("static").__getattribute__(name)
+
+        def __setattr__(self, name, value):
+            try:
+                object.__getattribute__(super().__getattribute__("static"), name)
+                raise Exception(f'A static variable named "{name}" already exists.')
+            except AttributeError:
+                super().__getattribute__("instance").__setattr__(name, value)
 
     def newInit(self):
         copiedInit(self)
@@ -56,7 +77,6 @@ def __parametrized(dec):
 @__parametrized
 def method(func, cls):
     setattr(cls, func.__name__, func)
-
     def inner(*args, **kwargs):
         return func(*args, **kwargs)
 
