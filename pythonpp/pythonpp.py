45dfb00c5b364417a53fa517b23c0044c3871097
Kento Nishi <kento24gs@outlook.com>
2020-06-08 22:19:57 -0700

    Partially working static variables and methods

--- pythonpp/pythonpp.py
+++ pythonpp/pythonpp.py
@@ -28,13 +28,10 @@ class __PrivateScope:
 
 
 def PythonPP(cls):
-    copiedInit = __copyMethod(cls.__init__)
-    staticPrivateScope = __PrivateScope()
-
-    class ScopeWrapper:
+    class Wrapper:
         pass
 
-    class Scope(ScopeWrapper):
+    class Scope(Wrapper):
         def __init__(self, instance, static):
             super().__setattr__("instance", instance)
             super().__setattr__("static", static)
@@ -50,15 +47,40 @@ def PythonPP(cls):
         def __setattr__(self, name, value):
             try:
                 object.__getattribute__(super().__getattribute__("static"), name)
-                raise Exception(f'A static variable named "{name}" already exists.')
+                super().__getattribute__("static").__setattr__(name, value)
             except AttributeError:
                 super().__getattribute__("instance").__setattr__(name, value)
 
+    copiedGetAttr = None
+    copiedInit = __copyMethod(cls.__init__)
+
+    def newGetAttr(self, name):
+        print(name)
+        try:
+            return object.__getattribute__(
+                object.__getattribute__(self, "__class__"), name
+            )
+        except AttributeError:
+            return copiedGetAttr(self, name)
+
+    def newSetAttr(self, name, value):
+        try:
+            object.__getattribute__(object.__getattribute__(self, "__class__"), name)
+            object.__getattribute__(self, "__class__").__setattr__(name, value)
+        except AttributeError:
+            object.__setattr__(self, name, value)
+
     def newInit(self):
+        nonlocal copiedInit, copiedGetAttr
         copiedInit(self)
+        staticPublicScope = self.__class__
+        staticPrivateScope = __PrivateScope()
         publicScope = Scope(self, self.__class__)
         privateScope = Scope(__PrivateScope(), staticPrivateScope)
         self.__class__.namespace(publicScope, privateScope)
+        copiedGetAttr = __copyMethod(self.__getattribute__)
+        self.__getattribute__ = newGetAttr
+        self.__setattr__ = newSetAttr
 
     cls.__init__ = newInit
     return cls
@@ -77,6 +99,7 @@ def __parametrized(dec):
 @__parametrized
 def method(func, cls):
     setattr(cls, func.__name__, func)
+
     def inner(*args, **kwargs):
         return func(*args, **kwargs)
 
