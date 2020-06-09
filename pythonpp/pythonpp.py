ac68c3d10036a1c09ff5a1d41547c9a6d8d9c392
Kento Nishi <kento24gs@outlook.com>
2020-06-09 15:55:13 -0700

    Added docstrings

--- pythonpp/pythonpp.py
+++ pythonpp/pythonpp.py
@@ -5,14 +5,33 @@ import traceback
 
 
 def __constructorCallback(func):
+    """
+    Called by the constructor.
+    Dynamically replaced by PythonPP.
+    """
     pass
 
 
 def constructor(func):
+    """
+    Constructor Wrapper for Python++ classes
+    The decorator does not take any arguments.
+    The scope is assumed to be a public instance method.
+
+    Example:
+    
+    @constructor
+    def __init__(variable1, variable2):
+        private.variable1 = variable1
+        private.static.variable2 = variable2
+    """
     __constructorCallback(func)
 
 
 def __copyMethod(f):
+    """
+    Creates a clone of a given method.
+    """
     try:
         g = types.FunctionType(
             f.__code__,
@@ -28,16 +47,11 @@ def __copyMethod(f):
     return g
 
 
-class __PrivateScope:
-    def __init__(self, values=dict()):
-        for name, value in values.items():
-            setattr(self, name, value)
-
-    def __setattr__(self, name, value):
-        object.__setattr__(self, name, value)
-
-
 def __parametrized(dec):
+    """
+    Allows existence of function wrappers with arguments which can have functions with arguments.
+    """
+
     def layer(*args, **kwargs):
         def repl(f):
             return dec(f, *args, **kwargs)
@@ -48,12 +62,48 @@ def __parametrized(dec):
 
 
 def PythonPP(cls):
+    """
+    The class wrapper to create Python++ objects.
+    Use this decorator on a class to declare it as a Python++ class.
+
+    Example:
+
+    @PythonPP
+    class Test:
+        def namespace(public, private):
+            public.static.testVariable = "Hello World!"
+    """
+
+    class __PrivateScope:
+        """
+        A private scope type.
+        This is just an object but it can set attributes with dot notation.
+        """
+
+        def __init__(self, values=dict()):
+            for name, value in values.items():
+                setattr(self, name, value)
+
+        def __setattr__(self, name, value):
+            object.__setattr__(self, name, value)
+
     class Wrapper:
+        """
+        An empty wrapper for scopes and data carrier classes.
+        """
+
         pass
 
     initDone = False
+    # signals if the constructor of the class finished executing.
 
     class Scope(Wrapper):
+        """
+        A Scope type which defaults attributes to instance.
+        
+        eg. If one gets an attribute, it will return instance.attribute
+        """
+
         def __init__(self, instance, static):
             super().__setattr__("instance", instance)
             super().__setattr__("static", static)
@@ -90,9 +140,14 @@ def PythonPP(cls):
             if initDone or not hasattr(super().__getattribute__("static"), name):
                 object.__setattr__(super().__getattribute__("static"), name, value)
 
-    userConstructor = None
+    userConstructor = lambda *args, **kwargs: None
 
-    def newConstructorDecorator(func):
+    def newConstructorCallback(func):
+        """
+        The replacement constructor decorator that takes the place of
+        __constructorCallback. userConstructor is set to the function passed
+        to the @constructor decorator.
+        """
         nonlocal userConstructor
         userConstructor = func
 
@@ -109,8 +164,11 @@ def PythonPP(cls):
     builtinAttr = False
 
     def getAttr(self, name):
+        """
+        A get attribute function which doesn't allow access to static variables from instance methods.
+        """
         nonlocal builtinAttr
-        if (name.startswith("__")) and (name.endswith("__")):
+        if name.startswith("__") and name.endswith("__"):
             builtinAttr = True
         if (not builtinAttr) and hasattr(cls, name):
             raise AttributeError("You cannot access static variables from an instance.")
@@ -119,11 +177,14 @@ def PythonPP(cls):
         return result
 
     def newInit(self, *args, **kwargs):
-
-        nonlocal copiedInit, userConstructor, newConstructorDecorator
+        """
+        The replacement __init__ function for the user's class.
+        Executes the original constructor, call
+        """
+        nonlocal copiedInit, userConstructor, newConstructorCallback, initDone
         global __constructorCallback
 
-        __constructorCallback = newConstructorDecorator
+        __constructorCallback = newConstructorCallback
 
         copiedInit(self)
 
@@ -146,6 +207,17 @@ def PythonPP(cls):
 
 @__parametrized
 def method(func, cls):
+    """
+    Used to expose methods to a particular scope
+
+    Example use case:
+    @method(public)
+    def publicmethod(*args, **kwargs):
+        for arg in args:
+            print(arg)
+
+    :param cls - the scope which the method is exposed to
+    """
     setattr(cls, func.__name__, func)
 
     def inner(*args, **kwargs):
